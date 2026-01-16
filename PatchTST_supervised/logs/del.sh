c264477540ae8b5208e3e6d6d6d1345a928b0655
root <root@juxin8-0.juxin8.podvm.svc.cluster.local>
2026-01-16 15:57:50 +0800

    add bayesian result

--- PatchTST_supervised/logs/del.sh
+++ PatchTST_supervised/logs/del.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type f -name "result.txt" | while read -r file; do
+find . -type f -name "result.txt" -path "*" | while read -r file; do
     total_lines=$(wc -l < "$file")
 
     # 文件行数小于2时跳过
@@ -9,7 +9,7 @@ find . -type f -name "result.txt" | while read -r file; do
     fi
 
     # 计算要删除的行号：倒数第二行
-    delete_line=$((total_lines))
+    delete_line=$((total_lines - 1))
 
     # 使用 sed 原地删除
     sed -i "${delete_line}d" "$file"
